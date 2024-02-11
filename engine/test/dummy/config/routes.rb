e6779bc678e774a80fae8d155a91237d2dafa197
Owais <owaiswiz@gmail.com>
2024-02-11 22:41:36 +0530

    remove dummy, install migration, setup devise

--- engine/test/dummy/config/routes.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.routes.draw do
-  mount Preflex::Engine => "/preflex"
-end
--- example/db/schema.rb
+++ example/db/schema.rb
@@ -10,5 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 0) do
+ActiveRecord::Schema[7.1].define(version: 2024_02_11_171238) do
+  create_table "preflex_preferences", force: :cascade do |t|
+    t.string "type"
+    t.string "owner_type"
+    t.bigint "owner_id"
+    t.text "data", limit: 16777217
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["type", "owner_type", "owner_id"], name: "index_preflex_preferences_on_type_and_owner_type_and_owner_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  end
+
 end
