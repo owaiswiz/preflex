cee748650d1bbcec11902809baad41a9fa32a127
Owais <owaiswiz@gmail.com>
2024-02-13 01:49:45 +0530

    yeet

---
+++ example/config/initializers/preflex.rb
@@ -0,0 +1,5 @@
+# If you have a custom base controller, uncomment the below line and set it here.
+#Preflex.base_controller_class = '::ApplicationController'
+
+# If you want to make it so that the controller that handles requests to update preferences(from the client-side) inherits from a different base controller, uncomment the below line and set it here.
+#Preflex.base_controller_class_for_update = '::ApplicationController'
