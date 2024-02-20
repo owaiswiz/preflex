75974dacb417b5f3e2c4c62e6b1c2d38db3354c5
Owais <owaiswiz@gmail.com>
2024-02-20 21:28:45 +0530

    use initializer

--- engine/lib/preflex/engine.rb
+++ engine/lib/preflex/engine.rb
@@ -4,9 +4,11 @@ module Preflex
   class Engine < ::Rails::Engine
     isolate_namespace Preflex
 
-    config.to_prepare do
-      Preflex.base_controller_class.include(Preflex::SetCurrentContext)
-      Preflex.base_controller_class_for_update.include(Preflex::SetCurrentContext) unless Preflex.base_controller_class_for_update < Preflex.base_controller_class
+    initializer 'preflex.set_current_context' do |app|
+      app.config.to_prepare do
+        Preflex.base_controller_class.include(Preflex::SetCurrentContext)
+        Preflex.base_controller_class_for_update.include(Preflex::SetCurrentContext) unless Preflex.base_controller_class_for_update < Preflex.base_controller_class
+      end
     end
   end
 end
