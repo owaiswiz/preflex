d7c331235fafe76bb4b8f5f67144a647b6c66eff
Owais <owaiswiz@gmail.com>
2024-02-20 22:35:19 +0530

    another attempt

--- engine/lib/preflex/engine.rb
+++ engine/lib/preflex/engine.rb
@@ -5,9 +5,15 @@ module Preflex
     isolate_namespace Preflex
 
     initializer 'preflex.set_current_context' do |app|
-      app.reloader.to_prepare do
+      include_current_context = -> {
         Preflex.base_controller_class.include(Preflex::SetCurrentContext)
         Preflex.base_controller_class_for_update.include(Preflex::SetCurrentContext) unless Preflex.base_controller_class_for_update < Preflex.base_controller_class
+      }
+      app.config.after_initialize do
+        include_current_context.call
+        app.reloader.to_prepare do
+          include_current_context.call
+        end
       end
     end
   end
